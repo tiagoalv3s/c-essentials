ee0c1473d924bd8fd2059ba6a1b2f0163dd1d6a5
tiagoalv3s <t.alv3s1986@gmail.com>
2022-07-01 18:27:06 -0300

    Update LAB:1.1.2.7

--- Module-1/LAB:1.1.2.7/main.c
+++ Module-1/LAB:1.1.2.7/main.c
@@ -1,5 +1,7 @@
 /*
- Check the program in the editor. Find all possible compilation errors and logic errors. Fix them. Your version of the program must print the same result as the expected output. Before you use the compiler, try to find the errors only by manual code analysis.
+ Check the program in the editor. Find all possible compilation errors and logic errors. Fix them.
+ Your version of the program must print the same result as the expected output.
+ Before you use the compiler, try to find the errors only by manual code analysis.
 
  #include <stdio.h>
 
