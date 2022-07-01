89bd20a7cdf421a272c501fc1d90807dbc6b288a
tiagoalv3s <t.alv3s1986@gmail.com>
2022-07-01 18:32:35 -0300

    Update LAB:1.1.2.7

--- Module-1/LAB:1.1.2.7/main.c
+++ Module-1/LAB:1.1.2.7/main.c
@@ -2,7 +2,7 @@
 - Check the program in the editor. Find all possible compilation errors and logic errors. Fix them.
 - Your version of the program must print the same result as the expected output.
 - Before you use the compiler, try to find the errors only by manual code analysis.
-Expected output:
+- Expected output:
 The value of five is: 5
 
 #include <stdio.h>
@@ -21,3 +21,4 @@ int main(void)
     printf("The value of five is: %d", 5);
     return 0;
 }
+
