7b625b6aecd8c703c6b8e03e9e03b821feb710ff
tiagoalv3s <t.alv3s1986@gmail.com>
2022-07-11 14:49:17 -0300

    Add LABs 2.1.4.8 to 2.1.4.10

---
+++ Module-2/LAB:2.1.4.10/main.c
@@ -0,0 +1,14 @@
+/*
+- Expected output:
+The day of the week is: Monday
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int dayOfWeek = 1;
+    if(dayOfWeek==1)
+        printf("The day of the week is: Monday\n");
+    return 0; 
+}
---
+++ Module-2/LAB:2.1.4.9/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = -3;
+    if(n < 3)
+        printf("The absolute value of %d is: %d\n", n, -n);
+        printf("The value of n is %d\n", n);
+    return 0;
+}
