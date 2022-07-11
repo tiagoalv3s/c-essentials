3b1b5a88fc20441e0cb6ccbdc9137c6734fcc1ba
tiagoalv3s <t.alv3s1986@gmail.com>
2022-07-11 10:38:31 -0300

    Update LAB:2.1.3.8

--- Module-2/LAB:2.1.3.8/main.c
+++ Module-2/LAB:2.1.3.8/main.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-    printf("Diff between 'c' and 'a' is: %d\n", 2);
-    printf("Diff between 'c' and 'c' is: %d\n", -2);
+    char c = 'c';
+    char a = 'a';
+    printf("Diff between \'%c\' and \'%c\' is: %d\n", c, a, c - a);
+    printf("Diff between \'%c\' and \'%c\' is: %d\n", a, c, a - c);
     return 0;
 }
