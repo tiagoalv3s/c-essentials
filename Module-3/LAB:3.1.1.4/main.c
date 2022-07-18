c2fc036eeaea3f4dfe27725df63140dc21e5d658
tiagoalv3s <t.alv3s1986@gmail.com>
2022-07-18 13:48:42 -0300

    Add LAB:3.1.1.4 in Module-3 folder

---
+++ Module-3/LAB:3.1.1.4/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a = 9;
+
+    if (a == 9)
+        puts("First condition is true");
+    else
+        puts("First condition is false");
+    if (a > 9)
+        puts("Second condition is true");
+    else
+        puts("Second condition is false");
+    if (a <= 10)
+        puts("Third condition is true");
+    else
+        puts("Third condition is false");
+    return 0;
+}
+
